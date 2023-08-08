c7fded4f8bf089bbb09e3c86581be88356769509
Gilberto Hernandez G <ghernandez.9002@gmail.com>
2023-08-08 01:11:36 -0600

    task: setup database driver

---
+++ shared/src/androidMain/kotlin/com/gilbertohdz/translator_kmm/translate/data/local/DatabaseDriverFactory.kt
@@ -0,0 +1,16 @@
+package com.gilbertohdz.translator_kmm.translate.data.local
+
+import android.content.Context
+import com.gilbertohdz.translator_kmm.database.TranslateDatabase
+import com.squareup.sqldelight.android.AndroidSqliteDriver
+import com.squareup.sqldelight.db.SqlDriver
+
+actual class DatabaseDriverFactory(
+  private val context: Context
+) {
+  actual fun create(): SqlDriver {
+    return AndroidSqliteDriver(
+      TranslateDatabase.Schema, context, "translate.db"
+    )
+  }
+}
