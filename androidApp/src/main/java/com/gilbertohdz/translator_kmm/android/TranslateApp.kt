b89360750b35f96e998d37a28b1833f46c3abc16
Gilberto Hernandez G <ghernandez.9002@gmail.com>
2023-08-09 21:54:38 -0600

    task: setup dagger hilt configuration

---
+++ androidApp/src/main/java/com/gilbertohdz/translator_kmm/android/TranslateApp.kt
@@ -0,0 +1,8 @@
+package com.gilbertohdz.translator_kmm.android
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class TranslateApp: Application()
--- shared/src/iosMain/kotlin/com/gilbertohdz/translator_kmm/translate/data/remote/HttpClientFactory.kt
+++ shared/src/iosMain/kotlin/com/gilbertohdz/translator_kmm/translate/data/remote/HttpClientFactory.kt
@@ -5,7 +5,7 @@ import io.ktor.client.engine.darwin.Darwin
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 
-class HttpClientFactory {
+actual class HttpClientFactory {
   actual fun create(): HttpClient {
     return HttpClient(Darwin) {
       install(ContentNegotiation) {
